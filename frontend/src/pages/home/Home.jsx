7e6fb769024b96861347790271d0809f3cf49287
Logan Briceno <Logan.briceno1967@gmail.com>
2024-10-17 18:38:42 -0500

    Created the Tpp Sellers in the Home Page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 //This is the home page for the website that is being sent to router.jsx
 import Banner from "./Banner"
+import TopSellers from "./TopSellers"
 
 export const Home = () => {
   return (
     <>
     <Banner />
+    <TopSellers />
     </>
   )
 }
