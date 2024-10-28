25acd4b4d98244c1eb985f598320442d30ebd5b6
Logan Briceno <Logan.briceno1967@gmail.com>
2024-10-28 13:33:43 -0500

    Added the recommended page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 //This is the home page for the website that is being sent to router.jsx
 import Banner from "./Banner"
 import TopSellers from "./TopSellers"
+import Recommended from "./Recommended"
 
 export const Home = () => {
   return (
     <>
     <Banner />
     <TopSellers />
+    <Recommended />
     </>
   )
 }
